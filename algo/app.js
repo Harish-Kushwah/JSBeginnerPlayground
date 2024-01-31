46cdf9d155beee6c9c1a88d28da00c0f5b91b2d1
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-01-31 20:06:15 +0530

    SCAN algorithm added

--- algo/app.js
+++ algo/app.js
@@ -1,65 +1,174 @@
 const numbers = document.querySelector(".num1");
 const btn = document.querySelector(".btn");
 const headInput = document.querySelector(".head");
+const seekInput = document.querySelector(".seek");
+const selectedAlgorithm = document.getElementById("algo");
+const headDirection = document.getElementById("direction");
+const disk_size = document.getElementById("disk_size");
 
-console.log(numbers);
-const seekInput = document.querySelector(".seek")
+class DiskSchedulingAlgorithm 
+{
+    FCFS(arr, head) {
+        let seek_count = 0;
+        let distance, cur_track;
+        for (var i = 0; i < arr.length; i++) {
+            cur_track = arr[i];
+    
+            // Calculate absolute distance
+            distance = Math.abs(cur_track - head);
+    
+            // Increase the total count
+            seek_count += distance;
+    
+            // Accessed track is now new head
+            head = cur_track;
+    
+            seekInput.innerHTML = "<h4>Total Seek Count : " + seek_count + "</h4>"
+        }
+        return arr;
+    }
 
-function FCFS(arr, head) {
-    var seek_count = 0;
-    var distance, cur_track;
-    for (var i = 0; i < arr.length; i++) {
-        cur_track = arr[i];
-
-        // Calculate absolute distance
-        distance = Math.abs(cur_track - head);
-
-        // Increase the total count
-        seek_count += distance;
-
-        // Accessed track is now new head
-        head = cur_track;
+    SCAN(arr,head , direction , disk_size){
+        let seek_count = 0;
+        let distance, cur_track;
+        let left = [], right = [];
+        let seek_sequence = [];
+        let size = arr.length;
+       // let disk_size = 200;
+        // appending end values
+        // which has to be visited
+        // before reversing the direction
+        if (direction == "LEFT")
+            left.push(0);
+        else if (direction == "RIGHT")
+            right.push(disk_size - 1);
+ 
+        for (let i = 0; i < size; i++)
+        {
+            if (arr[i] < head)
+                left.push(arr[i]);
+            if (arr[i] > head)
+                right.push(arr[i]);
+        }
+ 
+        // sorting left and right vectors
+        left.sort(function(a, b){return a - b});
+        right.sort(function(a, b){return a - b});
+ 
+        // run the while loop two times.
+        // one by one scanning right
+        // and left of the head
+        let run = 2;
+        while (run-- >0)
+        {
+            if (direction == "LEFT")
+            {
+                for (let i = left.length - 1; i >= 0; i--)
+                {
+                    cur_track = left[i];
+ 
+                    // appending current track to seek sequence
+                    seek_sequence.push(cur_track);
+ 
+                    // calculate absolute distance
+                    distance = Math.abs(cur_track - head);
+ 
+                    // increase the total count
+                    seek_count += distance;
+ 
+                    // accessed track is now the new head
+                    head = cur_track;
+                }
+                direction = "RIGHT";
+            }
+            else if (direction == "RIGHT")
+            {
+                for (let i = 0; i < right.length; i++)
+                {
+                    cur_track = right[i];
+ 
+                    // appending current track to seek sequence
+                    seek_sequence.push(cur_track);
+ 
+                    // calculate absolute distance
+                    distance = Math.abs(cur_track - head);
+ 
+                    // increase the total count
+                    seek_count += distance;
+ 
+                    // accessed track is now new head
+                    head = cur_track;
+                }
+                direction = "LEFT";
+            }
+        }
 
         seekInput.innerHTML = "<h4>Total Seek Count : " + seek_count + "</h4>"
+
+        left.reverse()
+        const data = [...left ,  ...right];
+        console.log(data);
+        return data;
+
     }
 }
 
-// var arr = [176, 79, 34, 60,92, 11, 41, 114, 45, 67, 23, 90, 89];
-// var head = 50;
+let algo = new DiskSchedulingAlgorithm();
 
+selectedAlgorithm.addEventListener("change" , (event)=>{
 
+        if(selectedAlgorithm.value == "SCAN"){
+            headDirection.style.display = "block";
+            console.log(headDirection.value);
+        }
+        else{
+            headDirection.style.display = "none";
 
+        }
 
-
-// data = arr;
+})
 btn.addEventListener("click", (event) => {
  
     const myData = [];
     let data = numbers.value.split(",");
     let head = Number(headInput.value);
-    console.log(head);
-    FCFS(data, head);
+
+    let algorithm_name = selectedAlgorithm.value;
+    if(algorithm_name == "FCFS"){
+      data = algo.FCFS(data, head);
+    }
+    else if(algorithm_name == "SCAN"){
+        let diskSize = Number(disk_size.value); 
+        data = algo.SCAN(data,head , headDirection.value , diskSize);
+    }
+
+
+
+
     for (let i = 0; i < data.length; i++) {
         myData.push({ x: i, y: Number(data[i]), indexLabel: '' + data[i] });
     }
 
-    draw(myData);
-
+    draw(myData , algorithm_name);
 });
 
-
-const draw = function (data) {
+/**
+ * This function is used to display the graphs
+ * @param {*} data 
+ * @param {*} algorithm_name 
+ */
+const draw = function (data , algorithm_name) {
     CanvasJS.addColorSet("colors",
     [
-    "#901245"                
+    "#DC143C"              
     ]);
 const chart = new CanvasJS.Chart("chartContainer",
 {
     colorSet: "colors",
     title: {
-        text: "FCFS",
+        text: algorithm_name,
         lineThickness: 0.5,
-        fontColor:"blue",
+        fontColor:"navy",
         fontSize:14,
         fontStyle:"normal",
         margin:4,
@@ -85,10 +194,10 @@ const chart = new CanvasJS.Chart("chartContainer",
     },
     data: [
         {
-            showInLegend: true,
+            showInLegend: false,
             // axisXType: "secondary",
             markerType: "circle",
-            legendText: "FCFS",
+            legendText: algorithm_name,
             type: "line",
             dataPoints: data
         },
